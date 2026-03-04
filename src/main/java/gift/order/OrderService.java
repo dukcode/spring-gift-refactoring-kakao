def8fdc60e03cc9cd0d317ff8d8ae3f2fa0b473a
김덕윤 <duk9741@gmail.com>
2026-03-04 15:31:10 +0900

    refactor(option): delegate price calculation to Option domain object

--- src/main/java/gift/order/OrderService.java
+++ src/main/java/gift/order/OrderService.java
@@ -43,7 +43,7 @@ public class OrderService {
         option.subtractQuantity(quantity);
         optionRepository.save(option);
 
-        int price = option.getProduct().getPrice() * quantity;
+        int price = option.calculateTotalPrice(quantity);
         member.deductPoint(price);
         memberRepository.save(member);
 
